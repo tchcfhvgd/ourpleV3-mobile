38083aff77b0713ad7a51970034e5bfb8689191e
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-05 13:26:05 +0800

    Update ChromaticAbb.hx

--- source/shaders/ChromaticAbb.hx
+++ source/shaders/ChromaticAbb.hx
@@ -22,16 +22,15 @@ class ChromaticAbb extends FlxShader
 		#pragma header
 
 		uniform float amount; //the point of this amount shit is so u dont have to put like a super tiny decimal to get it to look right
-		float offset = 0.01 * amount;
-		
 		void main()
 		{
+			float offset = 0.01 * amount;
 			vec4 col = flixel_texture2D(bitmap, openfl_TextureCoordv);
 			col.r = flixel_texture2D(bitmap, openfl_TextureCoordv.st - vec2(-offset, 0.0)).r;
 			col.b = flixel_texture2D(bitmap, openfl_TextureCoordv.st - vec2(offset, 0.0)).b;
 
 			gl_FragColor = col;
-		}
+}
 	')
 	public function new()
 	{
